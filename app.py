4db619d855bce7b190641f21bb2f44b1eb30188f
weeliangng <weeliangng@hotmail.com>
2020-04-02 00:27:47 +0800

    set up basic layout and utilities.py for sql queries

--- app.py
+++ app.py
@@ -3,29 +3,30 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
+from utilities import get_occupation_onetsocCode_list
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children="Hello Dash"),
+    html.Div(className="two columns",
+        children=[
+        html.Div(children=[
+                html.Label("Add Occupation"),
+                dcc.Dropdown(id="occupation_dropdown",
+                    options=get_occupation_onetsocCode_list()),
+                html.Button(id="add_occupation_node", children ="Add")
+        ]),
 
-    html.Div(children=[
-            html.Label("Add Occupation"),
-            dcc.Dropdown(id="occupation_dropdown",
-                options=[
-                {'label': 'New York City', 'value': 'NYC'},
-                {'label': 'Montreal', 'value': 'MTL'},
-                {'label': 'San Francisco', 'value': 'SF'}
-                ]),
-            html.Button(id="add_occupation_node", children ="Add")
+        html.Div(children=[
+            html.H1(id ="occupations")
+
+        ])
     ]),
 
-    html.Div(children=[
-        html.H1(id ="occupations")
-
-    ])
-
+    html.Div(className="eight columns",
+        children=[html.H1(children="Hello Dash")])
 
 ])
 
---
+++ utilities.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def get_occupation_onetsocCode_list():
+    conn = sqlite3.connect('./data/ONET.sqlite')
+    onetsoc_occupation_sql = """select distinct career_changers_matrix.onetsoc_code, title as occupation from career_changers_matrix 
+                            inner join occupation_data on career_changers_matrix.onetsoc_code = occupation_data.onetsoc_code;"""
+    c = conn.cursor()
+    data = []
+    for row in c.execute(onetsoc_occupation_sql):
+        data.append({
+            "label": row[1],
+            "value": row[0]
+        })
+    conn.close()
+    return data
