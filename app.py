dbe79f485247219a7dff916700376dd692403d9c
weeliangng <weeliangng@hotmail.com>
2020-04-05 11:08:14 +0800

    fix last click with timestamp

--- app.py
+++ app.py
@@ -24,15 +24,34 @@ app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 #edges = [{'data': {'source': 'one', 'target': 'two','label': 'Node 1 to 2'}},
 #            {'data': {'source': 'two', 'target': 'one','label': 'Node 2 to 1'}}
 #        ]
+cyto.load_extra_layouts()
 
 app.layout = html.Div(children=[
     html.Div(className="two columns",
         children=[
         html.Div(children=[
+                html.Label("Layout"),
+                dcc.Dropdown(id="layout_dropdown",
+                    options=[
+                        {'label': 'breadthfirst', 'value': 'breadthfirst'},
+                        {'label': 'circle', 'value': 'circle'},
+                        {'label': 'concentric', 'value': 'concentric'},
+                        {'label': 'cola', 'value': 'cola'},
+                        {'label': 'cose', 'value': 'cose'},
+                        {'label': 'cose-bilkent', 'value': 'cose-bilkent'},
+                        {'label': 'dagre', 'value': 'dagre'},
+                        {'label': 'euler', 'value': 'euler'},
+                        {'label': 'grid', 'value': 'grid'},
+                        {'label': 'klay', 'value': 'klay'},
+                        {'label': 'spread', 'value': 'spread'}
+                    ],
+                    value="cola",
+                    clearable=False),
                 html.Label("Occupation"),
                 dcc.Dropdown(id="occupation_dropdown",
                     options=dropdown_occupations(),
-                    value="11-1011.00"),
+                    value="11-1011.00",
+                    clearable=False),
                 html.Div(style = {'marginBottom': 25, 'marginTop': 25}, children=[
                     html.H5("Add outward occupation graph"),
                     html.Label(children ="Related index filter for outward occupations"),
@@ -70,8 +89,10 @@ app.layout = html.Div(children=[
                             value=[1,5],
                             marks={i+1:str(i+1) for i in range(10)}
                     )
+                    
                 ]),
                 html.Button(id="add_in_occupation_node_button", children ="Add"),
+                html.Br(),
                 html.Button(id="reset_graph_button", children ="Reset"),
         ])
     ]),
@@ -81,7 +102,7 @@ app.layout = html.Div(children=[
         children=[
             cyto.Cytoscape(
                 id='network_graph',
-                layout={'name': 'circle'},
+                layout={'name': 'cola'},
                 style={'width': '100%', 'height': '100vh'},
                 boxSelectionEnabled = True,
                 stylesheet= [
@@ -111,40 +132,47 @@ app.layout = html.Div(children=[
 ])
 
 @app.callback(
-    Output(component_id='network_graph', component_property='elements'),
-    [Input(component_id='add_out_occupation_node_button', component_property='n_clicks'),
-    Input(component_id='add_in_occupation_node_button', component_property='n_clicks')],
-    [State(component_id='occupation_dropdown', component_property='value'),
+    [Output(component_id='network_graph', component_property='elements'),
+    Output(component_id='network_graph', component_property='layout')],
+    [Input(component_id='add_out_occupation_node_button', component_property='n_clicks_timestamp'),
+    Input(component_id='add_in_occupation_node_button', component_property='n_clicks_timestamp')],
+    [State(component_id='layout_dropdown', component_property='value'),
+    State(component_id='occupation_dropdown', component_property='value'),
     State(component_id='out_related_index_filter_slider', component_property='value'),
     State(component_id='distance_slider', component_property='value'),
     State(component_id='in_related_index_filter_slider', component_property='value'),
     State(component_id='network_graph', component_property='elements')]
 )
-def add_occupation(add_out_occupation_node_button, add_in_occupation_node_button, onetsoc_code, out_related_index_filter, cutoff_distance, in_related_index_filter, existing_elements):
+def add_occupation(add_out_occupation_node_button, add_in_occupation_node_button, layout, onetsoc_code, out_related_index_filter, cutoff_distance, in_related_index_filter, existing_elements):
     elements = []
-    if add_out_occupation_node_button is None or add_in_occupation_node_button is None:
-        return dash.no_update
-    if add_out_occupation_node_button > 0:
+    if add_out_occupation_node_button is None and add_in_occupation_node_button is None:
+        return elements, {'name': 'circle'}
+    print(add_out_occupation_node_button)
+    print(add_in_occupation_node_button)
+    if add_out_occupation_node_button is None: add_out_occupation_node_button = 0
+    if add_in_occupation_node_button is None: add_in_occupation_node_button = 0
+    if add_out_occupation_node_button > add_in_occupation_node_button:
         elements = add_elements(onetsoc_code, related_index_filter = out_related_index_filter , cutoff = cutoff_distance)
         elements = [data for data in elements if data not in existing_elements]
         elements = elements + existing_elements
-        add_out_occupation_node_button = 0
-        add_in_occupation_node_button = 0
-    if add_in_occupation_node_button > 0:
+    if add_in_occupation_node_button > add_out_occupation_node_button:
         elements = add_elements(onetsoc_code, related_index_filter = in_related_index_filter , cutoff = 1, direction ="in")
         elements = [data for data in elements if data not in existing_elements]
         elements = elements + existing_elements
-        add_out_occupation_node_button = 0
-        add_in_occupation_node_button = 0
-    return elements
+    return elements, {'name': layout}
 
 @app.callback(
-    [Output(component_id='add_out_occupation_node_button', component_property='n_clicks'),
-    Output(component_id='add_in_occupation_node_button', component_property='n_clicks')],
+    [Output(component_id='add_out_occupation_node_button', component_property='n_clicks_timestamp'),
+    Output(component_id='add_in_occupation_node_button', component_property='n_clicks_timestamp')],
     [Input(component_id='reset_graph_button', component_property='n_clicks')]
 )
 def reset_graph(reset_graph_button):
-    return 0, 0
+    if reset_graph_button is None:
+        return dash.no_update, dash.no_update
+    elif  reset_graph_button >0:
+        add_out_occupation_node_n_clicks = None
+        add_in_occupation_node_n_clicks = None
+        return add_out_occupation_node_n_clicks, add_in_occupation_node_n_clicks
 
 @app.callback(
     [Output('about_occupation', 'children'),
